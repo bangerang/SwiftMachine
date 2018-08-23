9efceceac1dc269f55da8c5d8389f7f25c078b8c
Johan Thorell <jo1han@hotmail.com>
2018-08-23 16:29:22 +0200

    Initial Commit

---
+++ SwiftMachine/SwiftMachine.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMachine.h
+//  SwiftMachine
+//
+//  Created by Johan Thorell on 2018-08-23.
+//  Copyright © 2018 Johan Thorell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMachine.
+FOUNDATION_EXPORT double SwiftMachineVersionNumber;
+
+//! Project version string for SwiftMachine.
+FOUNDATION_EXPORT const unsigned char SwiftMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMachine/PublicHeader.h>
+
+
---
+++ SwiftMachineTests/SwiftMachineTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftMachineTests.swift
+//  SwiftMachineTests
+//
+//  Created by Johan Thorell on 2018-08-23.
+//  Copyright © 2018 Johan Thorell. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftMachine
+
+class SwiftMachineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
