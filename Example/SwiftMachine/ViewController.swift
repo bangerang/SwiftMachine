f0b68d7318fcab7968e181ba17694799cf2d064d
Johan Thorell <jo1han@hotmail.com>
2018-08-24 14:29:09 +0200

    Added example project

---
+++ Example/SwiftMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMachine
+//
+//  Created by Johan Thorell on 2018-08-24.
+//  Copyright © 2018 Johan Thorell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
