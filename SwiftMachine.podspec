0d8bcf4c177e9091c7bea08b8cc0476e29cf88dd
Johan Thorell <jo1han@hotmail.com>
2018-08-27 11:27:35 +0200

    Renamed ns pointer array function and some cocoapod configuration

--- SwiftMachine.podspec
+++ SwiftMachine.podspec
@@ -26,6 +26,7 @@ Pod::Spec.new do |s|
   s.source           = { :git => 'https://github.com/bangerang/SwiftMachine.git', :tag => s.version.to_s }
   s.social_media_url = 'https://twitter.com/johanthorell'
 
+  s.swift_version = '3.2'
   s.ios.deployment_target = '10.0'
 
   s.source_files = 'SwiftMachine/**/*'
--- SwiftMachine/NSPointerArray+Extension.swift
+++ SwiftMachine/NSPointerArray+Extension.swift
@@ -5,9 +5,7 @@
 //
 
 
-/*
-    Full credit for this extension to Marco Santarossa from his excellent blog post https://marcosantadev.com/swift-arrays-holding-elements-weak-references/
-*/
+
 import Foundation
 
 extension NSPointerArray {
@@ -20,16 +18,8 @@ extension NSPointerArray {
 		guard index < count, let pointer = self.pointer(at: index) else { return nil }
 		return Unmanaged<AnyObject>.fromOpaque(pointer).takeUnretainedValue()
 	}
-	
-	func removeObject(at index: Int) {
-		guard index < count else {
-			assert(false)
-			return
-		}
-		removePointer(at: index)
-	}
-	// NSPointerArray's own compact method doesnt work - https://gist.github.com/benvium/6854ed90c5bc3861afdd
-	func compact() {
+
+	func removeNilValues() {
 		for i in (0..<count).reversed() {
 			if object(at: i) == nil {
 				removePointer(at: i)
